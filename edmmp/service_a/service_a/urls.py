1e96d4e2576568d12d5827ec39fa58b64fcc92ab
Marius Räsener <marius.raesener@fincite.de>
2023-09-07 15:34:49 +0200

    add ruff

--- edmmp/service_a/service_a/urls.py
+++ edmmp/service_a/service_a/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
 from app_a.urls import urlpatterns as app_a_urls
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("app_a/", include(app_a_urls)),
